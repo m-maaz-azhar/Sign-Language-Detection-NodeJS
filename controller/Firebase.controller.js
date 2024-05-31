7fffb9d9531f1ef2961f7d3960c80581af38f071
Muhammad Maaz Azhar <maazazher28@gmail.com>
2024-05-31 18:32:27 +0500

    Update Firebase.controller.js

--- controller/Firebase.controller.js
+++ controller/Firebase.controller.js
@@ -3,7 +3,7 @@ const serviceAccount = require("../config/deaf-and-dumb-7017a-272c5113cb4d.json"
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "deaf-and-dumb-7017a.appspot.com",
+  storageBucket: "",
 });
 
 const bucket = admin.storage().bucket();
